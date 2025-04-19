9b024899f59fcb511844f851b770cfbde79b89ff
aayushk9 <ayushkokate1@gmail.com>
2025-04-19 22:35:07 +0530

    feat: auth with google is working at backend

---
+++ auth.ts
@@ -0,0 +1,53 @@
+import GoogleProvider from 'next-auth/providers/google'
+import { NextAuthOptions } from 'next-auth'
+import { prisma } from './lib/primsa'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.clientID || "",
+      clientSecret: process.env.clientSecret || ""
+    })
+  ],
+
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.email) return false
+
+      const exisitingUser = await prisma.user.findUnique({
+        where: { email: user.email }
+      })
+
+      if (!exisitingUser) {
+        await prisma.user.create({
+          data: {
+            id: user.id,
+            email: user.email,
+            provider: "Google"
+          }
+        })
+      }
+
+      return true;
+    },
+
+    async session({ session }) {
+      const dbUser = await prisma.user.findUnique({
+        where: { email: session?.user.email }
+      })
+
+      if (dbUser && session.user) {
+        session.user.id = dbUser.id
+      }
+
+      return session;
+    },
+
+  },
+  session: {
+    strategy: "jwt"
+  },
+
+
+  secret: process.env.NEXTAUTH_SECRET
+}
